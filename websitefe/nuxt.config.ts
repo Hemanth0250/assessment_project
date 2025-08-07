e4e327ac488cc4ffcbb9d2e3ca310808fd0c9ca5
Hemanth0250 <anuhemu25@gmail.com>
2025-08-07 09:48:37 +0530

    added tab icon and text

--- websitefe/nuxt.config.ts
+++ websitefe/nuxt.config.ts
@@ -2,12 +2,26 @@ import { defineNuxtConfig } from 'nuxt/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineNuxtConfig({
-  css: ['@/assets/css/tailwind.css',
+  css: [
+    '@/assets/css/tailwind.css',
     'bootstrap-icons/font/bootstrap-icons.css'
   ],
 
   vite: {
     plugins: [tsconfigPaths()]
+  },
+
+  app: {
+    head: {
+      title: 'dewi', // 🔹 Change Tab Title Here
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/png', href: 'https://cdn-icons-png.flaticon.com/512/2824/2824362.png' } // 🔹 Change Tab Icon Here
+      ]
+    }
   },
 
   modules: ['@nuxtjs/tailwindcss']
